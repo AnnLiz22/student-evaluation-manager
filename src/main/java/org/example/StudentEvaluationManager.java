ccecfd8f489b3e8a0ee7f7ec8bad79d7c89d56de
AnnLiz22 <bielakam@gmail.com>
2025-05-17 21:02:39 +0200

    refactor

--- src/main/java/org/example/StudentEvaluationManager.java
+++ src/main/java/org/example/StudentEvaluationManager.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 public class StudentEvaluationManager {
 
-public String evaluateStudentActivity(List<Student>students){
+public void evaluateStudentActivity(List<Student>students){
 
   for(Student student : students){
     if(student.hasSufficientPresence() && student.calculateWeightedAverage() >= 3.0){
       System.out.println("Student " + student.getName() + " " + student.getSurname()
-          + ", " + student.getYearOfStudies() + " : evaluation successful");
+          + ", " + student.getYearOfStudies() + " : evaluation successful. Student weighted average is: "
+          + student.calculateWeightedAverage());
     } else if(!student.hasSufficientPresence()){
       System.out.println("Student " + student.getName() + " " + student.getSurname()
         + " "  + student.getYearOfStudies() + " has to many absences!");
@@ -19,10 +20,5 @@ public String evaluateStudentActivity(List<Student>students){
     }
   }
 
-  return "Evaluation completed";
-
 }
-
-
-
 }
