84ef7dc7c3d23f58c69dd8ebe2289c45a5d627e4
AnnLiz22 <bielakam@gmail.com>
2025-05-17 21:29:19 +0200

    testing cases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,33 +5,44 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
 
     StudentEvaluationManager studentEvaluationManager = new StudentEvaluationManager();
+    List<Student> students = new ArrayList<>();
 
     Student student1 = new Student("Anna", "Kowalska", 3, 2);
-    Student student2 = new Student("Krzysztof", "Smith", 3, 5);
-    Student student3 = new Student("Krzysztof", "Smith", 3, 1);
+    Student student2 = new Student("Krzysztof", "Smith", 3, 1);
+    Student student3 = new Student("Kamil", "Adamski", 3, 5);
 
-    List <Grade> listOfGrades = List.of(
-        new Grade(5, 3, "n/a", true),
-    new Grade(4, 1, "n/a", true),
-    new Grade(1, 1, "n/a", true),
-    new Grade(5, 3, "n/a", true));
-
-    student1.addGrade(listOfGrades.get(0));
-    student1.addGrade(listOfGrades.get(1));
-    student1.addGrade(listOfGrades.get(2));
-    student1.addGrade(listOfGrades.get(3));
-
-    System.out.println("Average for student: " + student1.calculateAverage());
-    System.out.println("Weighted average for student: " + student1.calculateWeightedAverage());
-    System.out.println("Student has enough grades?" + student1.hasSufficientGrades());
-
-    List<Student> students = new ArrayList<>();
     students.add(student1);
     students.add(student2);
     students.add(student3);
+
+    student1.addGrade(new Grade(4.0,1,"ok", true));
+    student1.addGrade(new Grade(4.5,1,"ok", true));
+    student1.addGrade(new Grade(5, 3, "n/a", true));
+    student1.addGrade(new Grade(1, 1, "n/a", false));
+
+    student2.addGrade(new Grade(3.5,2,"ok", true));
+    student2.addGrade(new Grade(3,3,"ok", true));
+    student2.addGrade(new Grade(4,3,"ok", true));
+    student2.addGrade(new Grade(4, 1, "n/a", true));
+
+    System.out.println("Average for student: " + student1.getSurname() +
+       ": " + student1.calculateAverage());
+    System.out.println("Weighted average for student: "
+        + student1.getSurname() + ": " + student1.calculateWeightedAverage());
+    System.out.println("Student has enough grades? " + student1.hasSufficientGrades());
+
+    List<String> names = List.of("Mark", "Adam", "Patricia", "Alex");
+
+    List<Student> studentsWithName = names.stream()
+        .map(Student::new)
+        .toList();
+
+    students.addAll(studentsWithName);
+
+    System.out.println("Students by weighted average: " +
+        studentEvaluationManager.getStudentsByWeightedAverage(students));
 
     System.out.println(student1);
 
